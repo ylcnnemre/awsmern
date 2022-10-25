cb69c8f4a357170d77e356790bc0cbe13a58a317
emre <yalcnnemre@gmail.com>
2022-10-26 00:09:15 +0300

    add mongo

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,48 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors=require("cors")
 
-app.get("/",(req,res)=>{
-    res.json({
-        msg : "Hello Backend"
-    })
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/myfirstdb", (err) => {
+  if (!err) {
+    console.log("db connected");
+  } else {
+    console.log("db error");
+  }
+});
+
+const newSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const newModel = new mongoose.model("mycollect", newSchema);
+
+const getData = async () => {
+  const data = await newModel.find({});
+  return data;
+};
+
+app.get("/", async (req, res) => {
+  let sonuc = await getData();
+  res.send(sonuc);
+});
+
+
+app.post("/setdata",async (req,res)=>{
+
+    await newModel.insertMany([{
+         name : "leooo",
+         age: 35
+    }])
 })
 
 
-app.listen(3000)
+
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState();
 
-export default App
+  useEffect(() => {
+    (async () => {
+      const result = await fetch("http://localhost:3000");
+      const veri = await result.json();
+      setData(veri);
+      console.log("veri =>", veri);
+    })();
+  }, []);
+
+  const sendData = async () => {
+    /* const result = await fetch("http://localhost:3000/setdata", {
+      body: JSON.stringify({
+        name: "emreee",
+        age: 25,
+      }),
+      headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json123123123123'
+      },
+      method : "POST" */
+      axios.post("http://localhost:3000/setdata",{
+        "isim" : "emreeeee"
+      })
+    };
+
+   /*  
+    const getData=async()=>{
+       let sonuc= await axios.get("http://localhost:3000/")
+       
+
+    } */
+
+
+  return (
+    <div>
+      <button onClick={() => sendData()}>gönder</button>
+      {
+        data && data.map(val => <p> {val.name} </p>)
+      }
+    </div>
+  );
+};
+
+export default App;
